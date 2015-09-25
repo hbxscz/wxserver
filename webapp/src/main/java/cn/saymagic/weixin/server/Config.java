9f78ade8f5a3715d3777da2a12695f42b8dc5506
hbxscz <chenzhaoyy@gmail.com>
2015-09-25 23:47:04 +0800

    修改配置信息

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "";
+	public static final String TOKEN = "chenzhao82";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
+	public static final String APIKEY = "a641f7186c9b9f550508a566459b0cde"; 
 	
 }
