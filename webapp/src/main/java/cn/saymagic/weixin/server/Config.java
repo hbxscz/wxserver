91efed32f671abc4cd7c01f47b93e24187f415a9
hbxscz <chenzhaoyy@gmail.com>
2015-10-01 11:59:44 +0800

    修改微信token

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,7 +3,7 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "chenzhao82";
+	public static final String TOKEN = "chenzhao1984";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
 	public static final String APIKEY = "a641f7186c9b9f550508a566459b0cde"; 
